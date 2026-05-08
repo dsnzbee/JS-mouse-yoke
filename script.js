423c2e1b32d6ff90ee4106c55d6d51b7438f03c9
dsnzbee <parthpsh5@gmail.com>
2026-05-08 22:58:33 +0530

    Refactor mousemove event handling and styles

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+const debug = document.getElementById("debug");
 const yoke = document.getElementById("yoke");
+const sky = document.getElementById("blue");
+const ground = document.getElementById("orange");
+const ladder = document.getElementById("ladder");
 
-document.addEventListener("mousemove", (event) => {
+document.addEventListener("mousemove", function(event){
+  let x = event.clientX / window.innerWidth;
+  let y = event.clientY / window.innerHeight;
 
-  let mouseX = event.clientX;
+  let roll = (x - 0.5) * 35;
+  let pitch = (0.5 - y) * 100;
 
-  let rotation = (mouseX / window.innerWidth) * 60 - 30;
+  debug.innerHTML =
+   "X: " + Math.round(event.clientX) + "<br>" +
+   "Y: " + Math.round(event.clientY) + "<br>" +
+   "ROLL: " + Math.round(roll) + "DEG<br>" +
+   "PITCH: " + Math.round(pitch);
 
-  yoke.style.transform = `rotate(${rotation}deg)`;
+  yoke.style.left = event.clientX + "px";
+  yoke.style.top = event.clientY + "px";
 
+    ladder.style.transform = "translateY(" + pitch + "px) rotate(" + roll + "deg)";
 });
