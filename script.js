3b0b6b359daf21526cbf3754901be5d4a6602baf
dsnzbee <parthpsh5@gmail.com>
2026-05-08 15:58:20 +0000

    base JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const yoke = document.getElementById("yoke");
+
+document.addEventListener("mousemove", (event)=>{
+
+  let mouseX = event.clientX;
+  let mouseY = event.clientY;
+
+  console.log(mouseX, mouseY);
+
+});
