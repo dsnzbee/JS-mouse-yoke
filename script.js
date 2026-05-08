034927af7837907228fe3d895bd22421466960fd
dsnzbee <parthpsh5@gmail.com>
2026-05-09 00:50:34 +0530

    Refactor variable names and update transform logic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const debug = document.getElementById("debug");
 const yoke = document.getElementById("yoke");
-const sky = document.getElementById("blue");
-const ground = document.getElementById("orange");
+const blue = document.getElementById("blue");
+const orange = document.getElementById("orange");
 const ladder = document.getElementById("ladder");
 
 document.addEventListener("mousemove", function(event){
@@ -12,13 +12,16 @@ document.addEventListener("mousemove", function(event){
   let pitch = (0.5 - y) * 100;
 
   debug.innerHTML =
-   "X: " + Math.round(event.clientX) + "<br>" +
-   "Y: " + Math.round(event.clientY) + "<br>" +
-   "ROLL: " + Math.round(roll) + "DEG<br>" +
-   "PITCH: " + Math.round(pitch);
+    "X: " + Math.round(event.clientX) + "<br>" +
+    "Y: " + Math.round(event.clientY) + "<br>" +
+    "ROLL: " + Math.round(roll) + " DEG<br>" +
+    "PITCH: " + Math.round(pitch);
 
   yoke.style.left = event.clientX + "px";
   yoke.style.top = event.clientY + "px";
 
-    ladder.style.transform = "translateY(" + pitch + "px) rotate(" + roll + "deg)";
+  let move = "translateY(" + pitch + "px) rotate(" + roll + "deg)";
+  blue.style.transform = move;
+  orange.style.transform = move;
+  ladder.style.transform = move;
 });
