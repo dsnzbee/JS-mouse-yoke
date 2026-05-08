efec7d3a85af400d0670e51c85915a2dbb2c9843
dsnzbee <parthpsh5@gmail.com>
2026-05-08 21:52:53 +0530

    Update mousemove event to rotate element

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const yoke = document.getElementById("yoke");
 
-document.addEventListener("mousemove", (event)=>{
+document.addEventListener("mousemove", (event) => {
 
   let mouseX = event.clientX;
-  let mouseY = event.clientY;
 
-  console.log(mouseX, mouseY);
+  let rotation = (mouseX / window.innerWidth) * 60 - 30;
+
+  yoke.style.transform = `rotate(${rotation}deg)`;
 
 });
